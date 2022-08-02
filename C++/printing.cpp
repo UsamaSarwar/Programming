f1dbfca389d7632ada2e5e76e7d7c996a12f374d
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2022-08-02 19:44:46 +0500

    Printing

---
+++ C++/printing.cpp
@@ -0,0 +1,15 @@
+// Printing
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << 'U'; // Character 
+  cout <<"\n"; // New Line
+  cout << "Usama Sarwar";
+  cout << endl; // New Line
+  cout << "Lead Developer\n"; // New Line
+  cout << "Multiplication (90x5): "<< 90*5; // Concatenation
+  cout << endl;
+  cout << "Addition (90+5): "<< 90+5; 
+  return 0;
+}
---
+++ Dart/printing.dart
@@ -0,0 +1,8 @@
+main() {
+  print('U'); // Character
+  print("\n"); // New Line
+  print("Usama Sarwar");
+  print("Lead Developer\n"); // New Line
+  print("Multiplication (90x5):  ${90 * 5}"); // Concatenation
+  print("Addition (90+5): ${90 + 5}");
+}
