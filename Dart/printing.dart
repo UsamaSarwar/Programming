c7de3c52f8e320979b933510db43d884d3e69a1c
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2022-08-16 18:48:36 +0500

    Update printing.dart

--- Dart/printing.dart
+++ Dart/printing.dart
@@ -1,8 +1,16 @@
-main() {
+main(){
+  /// Printing Samples
+  print(786);
+  print(true);
   print('U'); // Character
   print("\n"); // New Line
   print("Usama Sarwar");
   print("Lead Developer\n"); // New Line
   print("Multiplication (90x5):  ${90 * 5}"); // Concatenation
   print("Addition (90+5): ${90 + 5}");
+  /*
+   Developed by Usama Sarwar
+   +9231 0000 777 3
+   contact@usama.dev
+  */
 }
