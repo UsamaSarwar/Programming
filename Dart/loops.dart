f85ac615deb448c9b8665c97fdd2342037866eff
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2022-08-29 19:37:37 +0500

    Create loops.dart

---
+++ Dart/loops.dart
@@ -0,0 +1,20 @@
+main() {
+  // for loop
+  for (int i = 0; i <= 9; i++) {
+    print('$i. Hello World');
+  }
+
+  // while loop
+  int j = 0;
+  while (j < 5) {
+    print('$j. Hello World');
+    j++;
+  }
+
+  // do while loop
+  int k = 0;
+  do {
+    print('$k. Hello World');
+    k++;
+  } while (k < 5);
+}
